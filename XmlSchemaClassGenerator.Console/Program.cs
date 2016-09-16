8150ab3150ab9a4a8b950381d688e3f6b3c815a6
Pretzelfisch <aaron@microfisch.com>
2016-09-16 11:22:55 -0700

    update to run program and process multiple commands.

--- XmlSchemaClassGenerator.Console/Program.cs
+++ XmlSchemaClassGenerator.Console/Program.cs
@@ -9,12 +9,64 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Mono.Options;
+using System.Runtime.InteropServices;
 
 namespace XmlSchemaClassGenerator.Console
 {
+
+
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main ( string[] args )
+        {
+
+            if ( args.Length > 0 )
+                RunCommand ( args );
+            else
+            {
+                bool exit = false;
+                do
+                {
+                    var command = System.Console.ReadLine();
+                    if ( command.ToLower () != "exit" )
+                    {
+                        RunCommand ( CommandLineToArgs ( command ) );
+                    }
+                    else
+                        exit = true;
+                } while ( exit == false );
+            }
+
+
+        }
+        public static string[] CommandLineToArgs ( string commandLine )
+        {
+            int argc;
+            var argv = CommandLineToArgvW(commandLine, out argc);
+            if ( argv == IntPtr.Zero )
+                throw new System.ComponentModel.Win32Exception ();
+            try
+            {
+                var args = new string[argc];
+                for ( var i = 0; i < args.Length; i++ )
+                {
+                    var p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
+                    args[ i ] = Marshal.PtrToStringUni ( p );
+                }
+
+                return args;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal ( argv );
+            }
+        }
+        [DllImport ( "shell32.dll", SetLastError = true )]
+        static extern IntPtr CommandLineToArgvW (
+[MarshalAs ( UnmanagedType.LPWStr )] string lpCmdLine, out int pNumArgs );
+
+        private static void RunCommand ( string[] args )
         {
             var showHelp = false;
             var namespaces = new List<string>();
@@ -34,66 +86,69 @@ namespace XmlSchemaClassGenerator.Console
             var codeTypeReferenceOptions = default(CodeTypeReferenceOptions);
 
             var options = new OptionSet {
-                { "h|help", "show this message and exit", v => showHelp = v != null },
-                { "n|namespace=", @"map an XML namespace to a C# namespace
-Separate XML namespace and C# namespace by '='.
-One option must be given for each namespace to be mapped.
-A file name may be given by appending a pipe sign (|) followed by a file name (like schema.xsd) to the XML namespace.
-If no mapping is found for an XML namespace, a name is generated automatically (may fail).", v => namespaces.Add(v) },
-                { "o|output=", "the {FOLDER} to write the resulting .cs files to", v => outputFolder = v },
-                { "i|integer=", @"map xs:integer and derived types to {TYPE} instead of string
-{TYPE} can be i[nt], l[ong], or d[ecimal].", v => {
-                                         switch (v)
-                                         {
-                                             case "i":
-                                             case "int":
-                                                 integerType = typeof(int);
-                                                 break;
-                                             case "l":
-                                             case "long":
-                                                 integerType = typeof(long);
-                                                 break;
-                                             case "d":
-                                             case "decimal":
-                                                 integerType = typeof(decimal);
-                                                 break;
-                                         }
-                                     } },
-                { "e|edb|enable-data-binding", "enable INotifyPropertyChanged data binding", v => enableDataBinding = v != null },
-                { "r|order", "emit order for all class members stored as XML element", v => emitOrder = v != null },
-                { "c|pcl", "PCL compatible output", v => pclCompatible = v != null },
-                { "p|prefix=", "the {PREFIX} to prepend to auto-generated namespace names", v => namespacePrefix = v },
-                { "v|verbose", "print generated file names on stdout", v => verbose = v != null },
-                { "0|nullable", "generate nullable adapter properties for optional elements/attributes w/o default values", v => nullables = v != null },
-                { "f|ef", "generate Entity Framework Code First compatible classes", v => entityFramework = v != null },
-                { "t|interface", "generate interfaces for groups and attribute groups (default is enabled)", v => interfaces = v != null },
-                { "a|pascal", "use Pascal case for class and property names (default is enabled)", v => pascal = v != null },
-                { "ct|collectionType=", "collection type to use (default is " + typeof(Collection<>).FullName + ")", v => collectionType = v == null ? typeof(Collection<>) : Type.GetType(v, true) },
-                { "cit|collectionImplementationType=", "the default collection type implementation to use (default is null)", v => collectionImplementationType = v == null ? null : Type.GetType(v, true) },
-                { "ctro|codeTypeReferenceOptions=", "the default CodeTypeReferenceOptions Flags to use (default is unset; can be: {GlobalReference, GenericTypeParameter})", v => codeTypeReferenceOptions = v == null ? default(CodeTypeReferenceOptions) : (CodeTypeReferenceOptions)Enum.Parse(typeof(CodeTypeReferenceOptions), v, false) }
-            };
+                    { "h|help", "show this message and exit", v => showHelp = v != null },
+                    { "n|namespace=", @"map an XML namespace to a C# namespace
+    Separate XML namespace and C# namespace by '='.
+    One option must be given for each namespace to be mapped.
+    A file name may be given by appending a pipe sign (|) followed by a file name (like schema.xsd) to the XML namespace.
+    If no mapping is found for an XML namespace, a name is generated automatically (may fail).", v => namespaces.Add(v) },
+                    { "o|output=", "the {FOLDER} to write the resulting .cs files to", v => outputFolder = v },
+                    { "i|integer=", @"map xs:integer and derived types to {TYPE} instead of string
+    {TYPE} can be i[nt], l[ong], or d[ecimal].", v => {
+                                             switch (v)
+                                             {
+                                                 case "i":
+                                                 case "int":
+                                                     integerType = typeof(int);
+                                                     break;
+                                                 case "l":
+                                                 case "long":
+                                                     integerType = typeof(long);
+                                                     break;
+                                                 case "d":
+                                                 case "decimal":
+                                                     integerType = typeof(decimal);
+                                                     break;
+                                             }
+                                         } },
+                    { "e|edb|enable-data-binding", "enable INotifyPropertyChanged data binding", v => enableDataBinding = v != null },
+                    { "r|order", "emit order for all class members stored as XML element", v => emitOrder = v != null },
+                    { "c|pcl", "PCL compatible output", v => pclCompatible = v != null },
+                    { "p|prefix=", "the {PREFIX} to prepend to auto-generated namespace names", v => namespacePrefix = v },
+                    { "v|verbose", "print generated file names on stdout", v => verbose = v != null },
+                    { "0|nullable", "generate nullable adapter properties for optional elements/attributes w/o default values", v => nullables = v != null },
+                    { "f|ef", "generate Entity Framework Code First compatible classes", v => entityFramework = v != null },
+                    { "t|interface", "generate interfaces for groups and attribute groups (default is enabled)", v => interfaces = v != null },
+                    { "a|pascal", "use Pascal case for class and property names (default is enabled)", v => pascal = v != null },
+                    { "ct|collectionType=", "collection type to use (default is " + typeof(Collection<>).FullName + ")", v => collectionType = v == null ? typeof(Collection<>) : Type.GetType(v, true) },
+                    { "cit|collectionImplementationType=", "the default collection type implementation to use (default is null)", v => collectionImplementationType = v == null ? null : Type.GetType(v, true) },
+                    { "ctro|codeTypeReferenceOptions=", "the default CodeTypeReferenceOptions Flags to use (default is unset; can be: {GlobalReference, GenericTypeParameter})", v => codeTypeReferenceOptions = v == null ? default(CodeTypeReferenceOptions) : (CodeTypeReferenceOptions)Enum.Parse(typeof(CodeTypeReferenceOptions), v, false) }
+                };
 
             var files = options.Parse(args);
 
-            if (showHelp)
+            if ( showHelp )
             {
-                ShowHelp(options);
+                ShowHelp ( options );
+
                 return;
             }
 
-            files = files.SelectMany(f => Glob.Glob.ExpandNames(f)).ToList();
+            files = files.SelectMany ( f => Glob.Glob.ExpandNames ( f ) ).ToList ();
 
             var namespaceMap = namespaces.Select(n => ParseNamespace(n, namespacePrefix)).ToNamespaceProvider(key =>
-            {
-                var xn = key.XmlSchemaNamespace;
-                var name = string.Join(".", xn.Split('/').Where(p => Regex.IsMatch(p, @"^[A-Za-z]+$") && p != "schema")
-                    .Select(n => Generator.ToTitleCase(n, NamingScheme.PascalCase)));
-                if (!string.IsNullOrEmpty(namespacePrefix)) name = namespacePrefix + (string.IsNullOrEmpty(name) ? "" : ("." + name));
-                return name;
-            });
+                {
+                    var xn = key.XmlSchemaNamespace;
+                    var name = string.Join(".", xn.Split('/').Where(p => Regex.IsMatch(p, @"^[A-Za-z]+$") && p != "schema")
+                        .Select(n => Generator.ToTitleCase(n, NamingScheme.PascalCase)));
+                    if (!string.IsNullOrEmpty(namespacePrefix))
+                        name = namespacePrefix + (string.IsNullOrEmpty(name) ? "" : ("." + name));
 
-            if (!string.IsNullOrEmpty(outputFolder))
-                outputFolder = Path.GetFullPath(outputFolder);
+                    return name;
+                });
+
+            if ( !string.IsNullOrEmpty ( outputFolder ) )
+                outputFolder = Path.GetFullPath ( outputFolder );
 
             var generator = new Generator
             {
@@ -111,7 +166,7 @@ If no mapping is found for an XML namespace, a name is generated automatically (
                 CodeTypeReferenceOptions = codeTypeReferenceOptions
             };
 
-            if (pclCompatible)
+            if ( pclCompatible )
             {
                 generator.UseXElementForAny = true;
                 generator.GenerateDesignerCategoryAttribute = false;
@@ -119,34 +174,33 @@ If no mapping is found for an XML namespace, a name is generated automatically (
                 generator.DataAnnotationMode = DataAnnotationMode.None;
             }
 
-            if (verbose) generator.Log = s => System.Console.Out.WriteLine(s);
+            if ( verbose ) generator.Log = s => System.Console.Out.WriteLine ( s );
 
-            generator.Generate(files);
+            generator.Generate ( files );
         }
-
-        static KeyValuePair<NamespaceKey, string> ParseNamespace(string nsArg, string namespacePrefix)
+        static KeyValuePair<NamespaceKey, string> ParseNamespace ( string nsArg, string namespacePrefix )
         {
             var parts = nsArg.Split(new[] {'='}, 2);
             var xmlNs = parts[0];
             var netNs = parts[1];
             var parts2 = xmlNs.Split(new[] {'|'}, 2);
             var source = parts2.Length == 2 ? new Uri(parts2[1], UriKind.RelativeOrAbsolute) : null;
-            xmlNs = parts2[0];
-            if (!string.IsNullOrEmpty(namespacePrefix))
+            xmlNs = parts2[ 0 ];
+            if ( !string.IsNullOrEmpty ( namespacePrefix ) )
                 netNs = namespacePrefix + "." + netNs;
-            return new KeyValuePair<NamespaceKey, string>(new NamespaceKey(source, xmlNs), netNs);
+            return new KeyValuePair<NamespaceKey, string> ( new NamespaceKey ( source, xmlNs ), netNs );
         }
 
-        static void ShowHelp(OptionSet p)
+        static void ShowHelp ( OptionSet p )
         {
-            System.Console.WriteLine("Usage: XmlSchemaClassGenerator.Console [OPTIONS]+ xsdFile...");
-            System.Console.WriteLine("Generate C# classes from XML Schema files.");
-            System.Console.WriteLine("Version " + typeof(Generator).Assembly.GetName().Version);
-            System.Console.WriteLine(@"xsdFiles may contain globs, e.g. ""content\{schema,xsd}\**\*.xsd"".");
-            System.Console.WriteLine(@"Append - to option to disable it, e.g. --interface-.");
-            System.Console.WriteLine();
-            System.Console.WriteLine("Options:");
-            p.WriteOptionDescriptions(System.Console.Out);
+            System.Console.WriteLine ( "Usage: XmlSchemaClassGenerator.Console [OPTIONS]+ xsdFile..." );
+            System.Console.WriteLine ( "Generate C# classes from XML Schema files." );
+            System.Console.WriteLine ( "Version " + typeof ( Generator ).Assembly.GetName ().Version );
+            System.Console.WriteLine ( @"xsdFiles may contain globs, e.g. ""content\{schema,xsd}\**\*.xsd""." );
+            System.Console.WriteLine ( @"Append - to option to disable it, e.g. --interface-." );
+            System.Console.WriteLine ();
+            System.Console.WriteLine ( "Options:" );
+            p.WriteOptionDescriptions ( System.Console.Out );
         }
     }
 }
